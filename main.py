fdbf26280022fdf9295174b3a5242251fde001e1
Delouya1 <delouya1@gmail.com>
2023-03-29 19:11:28 +0300

    Created the Article Summarizer using the nlp library and the sentiment analysis using TextBlob

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import tkinter as tk
+import nltk
+from textblob import TextBlob
+from newspaper import Article
+
+nltk.download('punkt')
+url = 'https://edition.cnn.com/2023/03/29/world/climate-change-photography-paul-nicklen-cristina-mittermeier-c2e-spc-intl-scn-climate/index.html'
+article = Article(url)
+
+article.download()
+article.parse()
+
+article.nlp()
+
+print(f'Article Title: {article.title}')
+print(f'Article Author: {article.authors}')
+print(f'Article Publish Date: {article.publish_date}')
+print(f'Article Summary: {article.summary}')
+
+
+# sentiment analysis
+analysis = TextBlob(article.text)
+print(f'Sentiment: {"Positive" if analysis.polarity > 0 else "Negative" if analysis.polarity < 0 else "Neutral"}')
+
+
+
+
+
 
